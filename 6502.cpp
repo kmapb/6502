b54d40566479860cc17d3944eb10bb5f5bc4214b
Keith Adams <kma@pebblebed.com>
2024-07-23 13:11:02 -0700

    w00tening continues.

--- 6502.cpp
+++ 6502.cpp
@@ -1,4 +1,45 @@
 #include <cstdio>
+#include <cstdint>
+
+struct Memory {
+    uint8_t bytes[1 << 16];
+};
+
+struct RegisterFile {
+    uint16_t PC;
+    uint8_t A;
+    uint8_t X;
+    uint8_t Y;
+    uint8_t SP;
+};
+
+enum AddressingMode {
+    ACCUMULATOR,
+    ABS,
+    ABS_X,
+    ABS_Y,
+    IMMEDIATE,
+    IMPLIED,
+    INDIRECT,
+    X_IND,
+    IND_Y,
+    REL,
+    ZPG,
+    ZPG_X,
+    ZPG_Y,
+};
+
+uint16_t
+operand(RegisterFile& regs, Memory& mem, AddressingMode mode) {
+   switch(mode) {
+      case ACCUMULATOR: return regs.A;
+      default: return 0;
+   }
+}
+
+#define OPCODES() \
+ OPCODE(BRK, IMPLIED) /* 0 */ \
+ OPCODE(ORA
 
 int main(int argc, char** argv) {
     printf("woot\n");
